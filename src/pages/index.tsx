22076b1a0fe2191c04a58eacf0aaccc7ad4f4da8
edmilson-dk <droidvidaboa@gmail.com>
2021-04-22 23:05:19 -0300

    feat: added header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Header } from '../components/Header';
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Next level week #5</h1>
+      <Header />
     </div>
   )
 }
