fd16fca8cac9235ede2d75ea9ba0448d5d6970d8
edmilson-dk <droidvidaboa@gmail.com>
2021-04-25 21:50:24 -0300

    feat: setted query params in json-server request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://localhost:3333/episodes");
+  const response = await fetch("http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc");
   const data = await response.json();
 
   return {
