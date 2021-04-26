ff5e5d79e56d820ddc5c46ede364dd4a053ec76b
edmilson-dk <droidvidaboa@gmail.com>
2021-04-26 19:41:03 -0300

    feat: added axios fetch api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+import api from "../services/api";
 
 type Episode = {
   id: string;
@@ -22,8 +26,23 @@ export default function Home(props: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await fetch("http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc");
-  const data = await response.json();
+  const { data } = await api.get("episodes?_limit=12&_sort=published_at&_order=desc", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc"
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MM yy', { locale: ptBR }),
+    }
+  })
 
   return {
     props: {
