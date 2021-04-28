bcc9364cf7bb3535b278cce2066998441e7134bb
edmilson-dk <droidvidaboa@gmail.com>
2021-04-28 20:28:48 -0300

    feat: added get static props next function for fetch epsiode data

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,11 +1,61 @@
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
+import api from "../../services/api";
+import { formatAudioDurationToString } from "../../utils/formatAudioDurationToString";
 
-export default function Episode() {
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string
+  publishedAt: string;
+  members: string;
+}
+
+type EpisodeProps = {
+  episode: Episode;
+}
+
+export default function Episode({ episode }: EpisodeProps) {
   const router = useRouter();
 
   return (
     <h1>
-      { router.query.slug }
+      { episode.title }
     </h1>
   );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { slug } = ctx.params;
+  const { data } = await api.get(`/episodes/${slug}`);
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(data.file.duration),
+    durationAsString: formatAudioDurationToString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+  }
+
+  return {
+    props: { episode },
+    revalidate: 60 * 60 * 24,
+  }
 }
