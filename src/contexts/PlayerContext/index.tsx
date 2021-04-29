71b1791c9a80ee84c61377ac3b23b0c7f056307f
edmilson-dk <droidvidaboa@gmail.com>
2021-04-29 20:20:16 -0300

    feat: created play method

--- src/contexts/PlayerContext/index.tsx
+++ src/contexts/PlayerContext/index.tsx
@@ -11,6 +11,7 @@ type Episode = {
 type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
+  play: (episode: Episode) => void;
 };
 
 type PlayerContextProviderProps = {
@@ -22,6 +23,11 @@ export const PlayerContext = createContext({} as PlayerContextData);
 export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
   const [ episodeList, setEpisodeList ] = useState([]);
   const [ currentEpisodeIndex, setCurrentEpisodeIndex ] = useState(0);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
 
   return (
     <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex }}>
