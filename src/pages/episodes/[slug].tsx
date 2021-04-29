f06cc642483499c103d959331b4b7ab1f2e1eeaf
edmilson-dk <droidvidaboa@gmail.com>
2021-04-28 21:12:09 -0300

    feat: finished episodes slug page html structure

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,9 +1,11 @@
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { GetStaticPaths, GetStaticProps } from "next";
-import { useRouter } from "next/router";
+import Image from "next/image";
 import api from "../../services/api";
 import { formatAudioDurationToString } from "../../utils/formatAudioDurationToString";
+
+import styles from "./episode.module.scss";
 
 type Episode = {
   id: string;
@@ -22,12 +24,32 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter();
-
   return (
-    <h1>
-      { episode.title }
-    </h1>
+    <section className={styles.episode}>
+      <div className={styles.thumbnailContainer}>
+        <button type="button">
+          <img src="/arrow-left.svg" alt="Voltar" />
+        </button>
+        <Image 
+          width={700}
+          height={160}
+          src={episode.thumbnail}
+          objectFit="cover"
+        />
+        <button type="button">
+          <img src="/play.svg" alt="Tocar episódio" />
+        </button>
+      </div>
+
+      <header>
+        <h1>{ episode.title }</h1>
+        <span>{ episode.members }</span>
+        <span>{ episode.publishedAt }</span>
+        <span>{ episode.durationAsString }</span>
+      </header>
+
+      <article className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }}/>
+    </section>
   );
 }
 
