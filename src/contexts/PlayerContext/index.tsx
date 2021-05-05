00c880d35b5d64a2cdde80a88d5641050f32465c
edmilson-dk <droidvidaboa@gmail.com>
2021-05-05 10:59:44 -0300

    feat: created togglePlay function and isPlaying state

--- src/contexts/PlayerContext/index.tsx
+++ src/contexts/PlayerContext/index.tsx
@@ -11,7 +11,9 @@ type Episode = {
 type PlayerContextData = {
   episodeList: Episode[];
   currentEpisodeIndex: number;
+  isPlaying: boolean;
   play: (episode: Episode) => void;
+  togglePlay: () => void;
 };
 
 type PlayerContextProviderProps = {
@@ -23,14 +25,20 @@ export const PlayerContext = createContext({} as PlayerContextData);
 export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
   const [ episodeList, setEpisodeList ] = useState([]);
   const [ currentEpisodeIndex, setCurrentEpisodeIndex ] = useState(0);
+  const [ isPlaying, setIsPlaying ] = useState(false);
 
   function play(episode: Episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, togglePlay, play, isPlaying }}>
       { children }
     </PlayerContext.Provider>
   )
