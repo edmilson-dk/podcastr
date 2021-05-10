a18e8e0a1821263e899eba1d2aa7cf1162b32abe
edmilson-dk <droidvidaboa@gmail.com>
2021-05-10 12:04:54 -0300

    feat: added title head tag

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { usePlayer } from "../../contexts/PlayerContext";
@@ -30,6 +31,10 @@ export default function Episode({ episode }: EpisodeProps) {
 
   return (
     <section className={styles.episode}>
+      <Head>
+        <title>{episode.title} - Home</title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
