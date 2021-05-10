af7340a4f9b83d1bf581f337fc30b5f9705cb09f
edmilson-dk <droidvidaboa@gmail.com>
2021-05-10 10:16:26 -0300

    feat: added hasNext and hasPrevious variables

--- src/contexts/PlayerContext/index.tsx
+++ src/contexts/PlayerContext/index.tsx
@@ -18,6 +18,8 @@ type PlayerContextData = {
   playList: (list: Episode[], index: number) => void;
   togglePlay: () => void;
   setPlayingState: (state: boolean) => void;
+  hasNext: boolean;
+  hasPrevious: boolean;
 };
 
 type PlayerContextProviderProps = {
@@ -51,16 +53,17 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsPlaying(state);
   }
 
+  const hasPrevious = currentEpisodeIndex > 0;
+  const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+
   function playNext() {
-    const nextEpisodeIndex = currentEpisodeIndex + 1;
-
-    if (nextEpisodeIndex >= episodeList.length) return;
-
-    setCurrentEpisodeIndex(nextEpisodeIndex);
+    if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+    }
   }
 
   function playPrevious() {
-    if (currentEpisodeIndex > 0) {
+    if (hasPrevious) {
       setCurrentEpisodeIndex(currentEpisodeIndex - 1);
     }
   }
@@ -75,7 +78,9 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
       playPrevious,
       playList,
       isPlaying, 
-      setPlayingState
+      setPlayingState,
+      hasNext,
+      hasPrevious,
     }}>
       { children }
     </PlayerContext.Provider>
